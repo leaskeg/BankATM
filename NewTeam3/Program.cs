f401150327b90a6f61dbabac080bab253b11fbd9
Jens Bach <leaskegaming@gmail.com>
2022-04-20 14:58:45 +0200

    Add project files.

---
+++ NewTeam3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewTeam3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
