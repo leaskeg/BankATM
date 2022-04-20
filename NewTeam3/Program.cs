5dec84e5d6acbfe32a1c8af7f66e0ce3eb2aec21
Jens Bach <leaskegaming@gmail.com>
2022-04-20 15:46:21 +0200

    Removed WriteLines

--- NewTeam3/Program.cs
+++ NewTeam3/Program.cs
@@ -6,7 +6,8 @@ namespace NewTeam3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Jacob!");
+            
         }
     }
 }
